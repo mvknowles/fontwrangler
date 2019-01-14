eb5bde033ee850bf5f21a252e52f1a403cdca8a4
Mark Knowles <mark@iceman.local>
2019-01-15 03:31:11 +1100

    Initial Commit

---
+++ fontwrangler/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  fontwrangler
+//
+//  Created by Mark Knowles on 15/1/19.
+//  Copyright © 2019 Mark Knowles <mark@mknowles.com.au>. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
